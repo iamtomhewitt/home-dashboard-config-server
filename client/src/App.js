5f241c08605d5ef2107ea0d0b898745535fa2629
Tom Hewitt <tom.hewitt@williamhill.com>
2021-02-14 15:38:11 +0000

    chore: initial frontend, fetching config

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    token: ''
+  }
+
+  getConfig = async () => {
+    const { token } = this.state;
+
+    if (token) {
+      const response = await fetch(`http://localhost:3001/config?token=${token}`);
+      const json = await response.json();
+      const { message, config } = json;
+
+      this.setState({ error: message, config })
+    }
+  }
+
+  extract = (obj) => {
+    const arr = []
+    Object.keys(obj).forEach(function (key) {
+      arr.push({ key, data: obj[key] })
+    });
+    return arr;
+  }
+
+  onTokenInputChange = event => {
+    const { id, value } = event.target;
+    this.setState({ [id]: value })
+  }
+
+  render() {
+    const { config, error } = this.state;
+
+    if (config) {
+      const { widgets, dialogs, endpoints } = config;
+
+      console.log('widgets', this.extract(widgets))
+      console.log('dialogs', this.extract(dialogs))
+      console.log('endpoints', this.extract(endpoints))
+
+      return (
+        <div >
+          <h1>Dashboard Config</h1>
+          <span>Token</span>
+          <input onChange={this.onTokenInputChange} id='token' />
+          <button onClick={this.onSubmitToken}>Submit</button>
+          <div>{JSON.stringify(widgets, null, 2)}</div>
+        </div>
+      );
+    }
+
+    return (
+      <div >
+        <h1>Dashboard Config</h1>
+        <span>Token</span>
+        <input onChange={this.onTokenInputChange} id='token' />
+        <button onClick={this.getConfig}>Submit</button>
+        {error && <div>Error: {error}</div>}
+      </div>
+    )
+  }
 }
 
 export default App;
