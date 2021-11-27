a10a090718968078f8f89743cd7799444055d7b8
Tom Hewitt <tommhewitt@gmail.com>
2021-11-27 14:25:09 +0000

    fix: heroku build

--- jest.setup.js
+++ jest.setup.js
@@ -2,3 +2,9 @@ import 'regenerator-runtime/runtime';
 import { configure } from '@testing-library/react';
 
 configure({ testIdAttribute: 'data-test-id' });
+
+global.MutationObserver = class {
+  constructor(callback) {}
+  disconnect() {}
+  observe(element, initObject) {}
+};
