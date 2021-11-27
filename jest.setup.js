088f0ef3b222669aa92f6c4db6b0b2eac082df95
Tom Hewitt <tommhewitt@gmail.com>
2021-11-27 14:34:39 +0000

    fix: heroku build

--- jest.setup.js
+++ jest.setup.js
@@ -1,10 +1,12 @@
+/* eslint-disable */
 import 'regenerator-runtime/runtime';
 import { configure } from '@testing-library/react';
 
 configure({ testIdAttribute: 'data-test-id' });
 
 global.MutationObserver = class {
-  constructor(callback) {}
-  disconnect() {}
-  observe(element, initObject) {}
+  constructor(callback) { }
+  disconnect() { }
+  observe(element, initObject) { }
 };
+/* eslint-enable */
