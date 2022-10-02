b6326b755c1f45b7f70c955024544217a4345ef7
Tom Hewitt <tommhewitt@gmail.com>
2022-10-02 12:31:46 +0100

    chore(#23): use firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, get, child, update } from 'firebase/database';
+
+const app = initializeApp({
+  databaseURL: process.env.REACT_APP_FIREBASE,
+});
+
+const db = ref(getDatabase(app));
+
+export const getData = async () => {
+  const json = await get(child(db, '/')).then((x) => x.val());
+  return json;
+};
+
+export const updateData = async ({ index, data }) => {
+  update(child(db, `/${index}`), data).then(console.log);
+};
